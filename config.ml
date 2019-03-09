53f114589bf428e6234ffd3a06d0c429e4be31d1
Hiroshi Doyu <hiroshi.doyu@ericsson.com>
2019-03-09 16:28:53 +0000

    seems OK but need to clean up

--- config.ml
+++ config.ml
@@ -1,21 +1,21 @@
 open Mirage
 
-let daddr =
-  let doc = Key.Arg.info ~doc:"destination address" ["daddr"] in
-  Key.(create "daddr" Arg.(opt string "127.0.0.1" doc))
+let dp =
+  let doc = Key.Arg.info ~doc:"The TCP port to send." ["dp"] in
+  Key.(create "dp" Arg.(opt string "5555" doc))
 
-let dport =
-  let doc = Key.Arg.info ~doc:"The TCP port to send." ["dport"] in
-  Key.(create "dport" Arg.(opt string "5555" doc))
+let sp =
+  let doc = Key.Arg.info ~doc:"The TCP port to send." ["sp"] in
+  Key.(create "sp" Arg.(opt string "6666" doc))
 
-let sport =
-  let doc = Key.Arg.info ~doc:"The TCP port to send." ["sport"] in
-  Key.(create "sport" Arg.(opt string "6666" doc))
+let ch =
+  let doc = Key.Arg.info ~doc:"string to send." ["ch"] in
+  Key.(create "ch" Arg.(opt string "x" doc))
 
 let keys = List.map Key.abstract [
-    sport;
-    daddr;
-    dport;
+    sp;
+    dp;
+    ch;
   ]
 
 let main = foreign ~keys "Unikernel.Main" (stackv4 @-> job)
--- unikernel.ml
+++ unikernel.ml
@@ -2,45 +2,73 @@ open Lwt.Infix
 
 module Main (S: Mirage_stack_lwt.V4) = struct
 
-  let start s =
-    let daddr = Key_gen.daddr () |> Ipaddr.V4.of_string_exn in
-    let dport = Key_gen.dport () |> int_of_string in
-    let sport = Key_gen.sport () |> int_of_string in
+  let sflow : S.TCPV4.flow option ref = ref None
 
-    let inc () =
-      S.TCPV4.create_connection (S.tcpv4 s) (daddr, dport) >>= function
-      | Error _err -> failwith "Connection to port failed"
+  let inc str =
+    (String.(get str 0 |> make 1)) ^ str
+
+  let start s =
+    let localhost = "127.0.0.1" in
+    let dst_addr = Ipaddr.V4.of_string_exn localhost in
+    let dst_port = Key_gen.dp () |> int_of_string in
+    let src_port = Key_gen.sp () |> int_of_string in
+    let ch = Key_gen.ch () in
+
+    let rec create_sflow count =
+      S.TCPV4.create_connection (S.tcpv4 s) (dst_addr, dst_port) >>= function
+      | Error _err ->
+        Unix.sleep 1;
+        Logs.debug (fun f -> f "retry(%d) to connect %s:%d" count localhost dst_port);
+        create_sflow (count+1)
       | Ok flow ->
-        let rec loop n =
-          S.TCPV4.write flow (Cstruct.of_string (string_of_int n)) >>= function
-          | Error _ -> assert false
-          | Ok () -> match n with
-            | 9 -> S.TCPV4.close flow
-            | _ -> loop (n+1)
-        in
-        loop 0
+        Logs.debug (fun f -> f "connected(%d) %s:%d, saving sflow...." count localhost dst_port);
+        sflow := Some flow;
+        Lwt.return_unit
+    in
+
+    let rec send str =
+      match !sflow with
+      | None -> create_sflow 0 >>= fun () -> send str
+      | Some fl ->
+        S.TCPV4.write fl (Cstruct.of_string str) >>= function
+        | Error _ -> assert false
+        | Ok () -> Lwt.return_unit
+    in
+
+    let rec reply str =
+      match !sflow with
+      | None ->
+        Logs.info (fun f -> f "retry to create sflow");
+        create_sflow 0 >>= fun _ -> reply str
+      | Some fl ->
+        let buf = inc str in
+        let addr, port = S.TCPV4.dst fl in
+        Logs.debug (fun f -> f "write@%s:%d %dB: %s" (Ipaddr.V4.to_string addr) port (String.length buf) buf);
+        S.TCPV4.write fl (Cstruct.of_string buf)
+    in
+
+    let rec callback flow =
+      S.TCPV4.read flow >>= function
+
+      | Ok `Eof -> Logs.info (fun f -> f "EOF"); Lwt.return_unit
+      | Error e -> Logs.warn (fun f -> f "Error reading data from established connection: %a" S.TCPV4.pp_error e); Lwt.return_unit
+      | Ok (`Data b) ->
+        let str = Cstruct.to_string b in
+        let addr, port = S.TCPV4.dst flow in
+        Logs.debug (fun f -> f "read@%s:%d %dB: %s" (Ipaddr.V4.to_string addr) port (Cstruct.len b) str);
+        Unix.sleep (Random.int 3);
+        reply str >>= function
+        | Error _ -> assert false
+        | Ok () -> callback flow
     in
 
     let listen () =
-      let rec callback flow =
-        let dst, dst_port = S.TCPV4.dst flow in
-        Logs.info (fun f -> f "new tcp connection from IP %s on port %d"
-                      (Ipaddr.V4.to_string dst) dst_port);
-        S.TCPV4.read flow >>= function
-        | Ok `Eof -> Logs.info (fun f -> f "Closing connection!"); Lwt.return_unit
-        | Error e -> Logs.warn (fun f -> f "Error reading data from established connection: %a" S.TCPV4.pp_error e); Lwt.return_unit
-        | Ok (`Data b) ->
-          Logs.debug (fun f -> f "read: %d bytes:\n%s" (Cstruct.len b) (Cstruct.to_string b));
-          (* S.TCPV4.close flow *)
-          callback flow
-      in
-
-      S.listen_tcpv4 s ~port:sport callback;
+      S.listen_tcpv4 s ~port:src_port callback;
       S.listen s;
     in
 
     Lwt.join [
-      inc ();
+      send ch;
       listen ();
     ]
 
