b316f01feeb372a22fd954ba00b215954dca2c57
Hiroshi Doyu <hiroshi.doyu@ericsson.com>
2019-03-18 06:59:39 +0000

    introduce marshal
    If the write out amount is less than 4096(4096=32+4064), marshal works ok.
    
      $ dd if=/dev/urandom of=t/secret bs=1 count=4064
      4064+0 records in
      4064+0 records out
      4064 bytes (4.1 kB, 4.0 KiB) copied, 0.0140433 s, 289 kB/s
      ehirdoy@ehirdoy-vm:~/src/work/marshal$ make && ./size -l "*:debug"
      mirage build
      2019-03-18 07:01:15 +00:00: INF [tcpip-stack-socket] Manager: connect
      2019-03-18 07:01:15 +00:00: INF [tcpip-stack-socket] Manager: configuring
      2019-03-18 07:01:15 +00:00: INF [application] Write out 4096 bytes name=test age=9 buf=4064
      2019-03-18 07:01:15 +00:00: INF [application] restoring name=test age=9 buf=4064
    
    If the write out amount is more than 4096(4097=32+4065), marshal doesn't work.
    
      $ dd if=/dev/urandom of=t/secret bs=1 count=4065
      4065+0 records in
      4065+0 records out
      4065 bytes (4.1 kB, 4.0 KiB) copied, 0.0159368 s, 255 kB/s
      ehirdoy@ehirdoy-vm:~/src/work/marshal$ make && ./size -l "*:debug"
      mirage build
      2019-03-18 07:01:22 +00:00: INF [tcpip-stack-socket] Manager: connect
      2019-03-18 07:01:22 +00:00: INF [tcpip-stack-socket] Manager: configuring
      2019-03-18 07:01:22 +00:00: INF [application] Write out 4097 bytes name=test age=9 buf=4065
      2019-03-18 07:01:22 +00:00: WRN [tcpip-stack-socket] error (Invalid_argument Marshal.from_bytes) in callback
    
    Signed-off-by: Hiroshi Doyu <hiroshi.doyu@ericsson.com>

--- unikernel.ml
+++ unikernel.ml
@@ -2,6 +2,12 @@ open Lwt.Infix
 
 module Main (S: Mirage_stack_lwt.V4) (R: Mirage_random.C) (KV: Mirage_kv_lwt.RO) = struct
 
+  type content = {
+    name : string;
+    age  : int;
+    buf  : bytes;
+  }
+
   let read_whole_file kv key =
     KV.size kv key >>= function
     | Error e -> Lwt.return @@ Error e
@@ -18,8 +24,10 @@ module Main (S: Mirage_stack_lwt.V4) (R: Mirage_random.C) (KV: Mirage_kv_lwt.RO)
                      (fun f -> f "Error reading data from established connection: %a"
                          S.TCPV4.pp_error e); Lwt.return_unit
       | Ok (`Data data) ->
-        let addr, port = S.TCPV4.dst flow in
-        Logs.debug (fun f -> f "read@%s:%d %d bytes" (Ipaddr.V4.to_string addr) port (Cstruct.len data));
+        let _addr, _port = S.TCPV4.dst flow in
+        Logs.info (fun f -> f "cbuf=%d total=%d" (Cstruct.len data) (Marshal.total_size (Cstruct.to_bytes data) 0));
+        let out = Marshal.from_string (Cstruct.to_string data) 0 in
+        Logs.info (fun f -> f "restoring name=%s age=%d buf=%d" out.name out.age (Bytes.length out.buf));
         callback flow
     in
     S.listen_tcpv4 s ~port:port callback;
@@ -29,16 +37,24 @@ module Main (S: Mirage_stack_lwt.V4) (R: Mirage_random.C) (KV: Mirage_kv_lwt.RO)
       S.TCPV4.create_connection (S.tcpv4 s) (localhost, port) >>= function
       | Error _err -> OS.Time.sleep_ns (Duration.of_sec 1) >>= fun () -> send_data ()
       | Ok flow ->
-        Logs.debug (fun f -> f "connected port %d" port);
         read_whole_file kv "secret" >>= function
         | Error e ->
           Logs.warn (fun f -> f "Could not compare the secret against a known constant: %a"
                         KV.pp_error e); Lwt.return_unit
         | Ok stored_secret ->
-          let buf = Cstruct.concat stored_secret in
-          S.TCPV4.write flow buf >>= function
+          let str =
+            Marshal.to_string
+              {name="test"; age=9; buf=(Cstruct.to_bytes (Cstruct.concat stored_secret));} []
+          in
+          let cbuf = Cstruct.of_string str in
+          S.TCPV4.write flow cbuf >>= function
           | Error _ -> assert false
-          | Ok () -> Logs.info (fun f -> f "Write out %d bytes" (Cstruct.len buf)); Lwt.return_unit
+          | Ok () ->
+            let str = Cstruct.to_string cbuf in
+            let orig = Marshal.from_string str 0 in
+            Logs.info (fun f -> f "Write out %d bytes name=%s age=%d buf=%d"
+                          (Cstruct.len cbuf) orig.name orig.age (Bytes.length orig.buf));
+            Lwt.return_unit
     in
 
     Lwt.join [
